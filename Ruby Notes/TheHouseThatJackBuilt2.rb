2df849f72421167833f7112ed52e9894681496cf
claudiawulee <cl7359@princeton.edu>
2023-07-18 20:42:42 -0400

    updating Ruby notes and checkpoint projects

--- Ruby Notes/TheHouseThatJackBuilt2.rb
+++ Ruby Notes/TheHouseThatJackBuilt2.rb
@@ -1,3 +1,35 @@
+
+class Jack
+    attr_reader :phrases
+    def initialize(phrases)
+        @phrases = phrases
+    end
+
+    def randomizeRhyme
+        #switching items in the array approach
+        puts "Enter the number of times you would like to randomize the array: "
+        num = gets.chomp.to_i
+        num.times do 
+            index1 = Random.rand(0...phrases.length())
+            index2 = Random.rand(0...phrases.length())
+            temp = phrases[index1]
+            phrases[index1] = phrases[index2]
+            phrases[index2] = temp
+        end
+    end
+    def printRhyme
+        randomizeRhyme
+        phrases[0] << "." #appends a period to the first sentence (so every sentence has a period)
+        nursery_rhyme = "" #final string containing all the lines
+        string = "" #keeps track of previous sentence(s) used to make new sentences
+        phrases.each do |phrase|
+            string = phrase + " " + string #creates new line
+            nursery_rhyme += "This is " + string + "\n" #adds to the collection of other lines
+        end
+        nursery_rhyme
+    end
+end
+
 phrases = []
 phrases.push("the house that Jack built")
 phrases.push("the malt that lay in")
@@ -12,26 +44,10 @@ phrases.push("the rooster that crowed in the morn that woke")
 phrases.push("the farmer sowing his corn that kept")
 phrases.push("the horse and the hound and the horn that belonged to")
 
-#switching items in the array approach
-puts "Enter the number of times you would like to randomize the array: "
-num = gets.chomp.to_i
-num.times do 
-    index1 = Random.rand(0...phrases.length())
-    index2 = Random.rand(0...phrases.length())
-    temp = phrases[index1]
-    phrases[index1] = phrases[index2]
-    phrases[index2] = temp
-end
+jack = Jack.new(phrases)
+puts jack.printRhyme
 
 
-phrases[0] << "." #appends a period to the first sentence (so every sentence has a period)
-nursery_rhyme = "" #final string containing all the lines
-string = "" #keeps track of previous sentence(s) used to make new sentences
-phrases.each do |phrase|
-    string = phrase + " " + string #creates new line
-    nursery_rhyme += "This is " + string + "\n" #adds to the collection of other lines
-end
-    puts nursery_rhyme
 
 
 
