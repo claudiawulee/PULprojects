8c5535581638d7ed217f6a342b8b22662522a826
claudiawulee <cl7359@princeton.edu>
2023-07-19 10:00:43 -0400

    reorganizing my notes

---
+++ Ruby/Notes/store.rb
@@ -0,0 +1,62 @@
+module Inventoryable
+    module ClassMethods
+        def create(attributes)
+            object = new(attributes)
+            instances.push(object)
+            return object
+        end
+        def instances 
+            @instances ||= []
+        end
+
+    end
+
+    def stock_count
+        @stock_count ||= 0
+    end
+    def stock_count=(number)
+        @stock_count = number
+    end
+    def in_stock?
+        stock_count > 0
+    end
+end
+
+
+class Shirt
+    extend Inventoryable::ClassMethods
+    include Inventoryable
+  attr_accessor :attributes
+  
+  def initialize(attributes)
+    @attributes = attributes
+  end
+end
+
+class Pant
+  attr_accessor :attributes
+  
+  def initialize(attributes)
+    @attributes = attributes
+  end
+end
+
+class Accessory
+  attr_accessor :attributes
+  
+  def initialize(attributes)
+    @attributes = attributes
+  end
+end
+
+shirt1 = Shirt.create(name: "MTF", size: "L")
+shirt2 = Shirt.create(name: "MTF", size: "M")
+
+shirt1.stock_count = 10
+puts "Shirt 1 stock count: %s" % shirt1.stock_count
+puts "Shirt 2 stock count: %s" % shirt2.stock_count
+
+puts "Shirt 1 in stock?: %s" % shirt1.in_stock?
+puts "Shirt 2 in stock?: %s" % shirt2.in_stock?
+
+
