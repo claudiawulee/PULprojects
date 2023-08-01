e1ec49642eb89478de17f33f237991f6f5c46a6c
claudiawulee <cl7359@princeton.edu>
2023-08-01 14:47:23 -0400

    Updating sinatra project

--- Ruby/sinatra_project/web.rb
+++ Ruby/sinatra_project/web.rb
@@ -13,3 +13,25 @@ get "/jack1" do
     @poem = jack1.original_rhyme()
     erb :jack1
 end
+
+get "/jack2" do
+    erb :jack2
+end
+
+post "/jack2" do
+    jack2 = Jack.new()
+    num = (params[:num]).to_i
+    @poem2 = jack2.random_rhyme(num)
+    erb :jack2_results
+end
+
+get "/jack3" do
+    erb :jack3
+end
+
+post "/jack3" do
+    jack3 = Jack.new()
+    num = (params[:num]).to_i
+    @poem3 = jack3.original_ending(num)
+    erb :jack3_results
+end
