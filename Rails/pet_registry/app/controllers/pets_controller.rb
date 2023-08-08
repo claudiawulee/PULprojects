623b9e600f34721a2bc8c9149b239adb4a0ad70b
claudiawulee <cl7359@princeton.edu>
2023-08-08 11:16:37 -0400

    adding checkpoint project and rails notes

---
+++ Rails/pet_registry/app/controllers/pets_controller.rb
@@ -0,0 +1,51 @@
+class PetsController < ApplicationController
+  def index
+    #fetches all pets from the database
+    @pets = Pet.all
+  end
+
+  def show
+    @pet = Pet.find(params[:id])
+  end
+
+  def new
+    @pet = Pet.new 
+  end
+
+  def create 
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to @pet
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    @pet = Pet.find(params[:id])
+
+    if @pet.update(pet_params)
+      redirect_to @pet
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @pet = Pet.find(params[:id])
+    @pet.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+  def pet_params
+      params.require(:pet).permit(:name, :age, :animal, :color, :breed)
+  end
+
+end
+
