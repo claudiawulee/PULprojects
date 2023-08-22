a8f5f9826180515b16e43b70604ae965ccae6fe7
claudiawulee <cl7359@princeton.edu>
2023-08-22 18:45:24 +0000

    add activities form

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,2 +1,18 @@
-alert("Hello World!");
-console.log("HI");
+// alert("Hello World!");
+// console.log("HI");
+// document.write("Hello");
+
+// var message = "hi";
+//catches if you redefine another variable of same name 
+let message = "hello \
+this continues on the next line \
+ and JS will understand.";
+const num = 6;
+
+console.log(message);
+
+// .length()
+// .toUpperCase()
+// .toLowerCase()
+
+// prompt("Message/question here")
--- Rails/pet_registry/config/routes.rb
+++ Rails/pet_registry/config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # get "/pets", to: "pets#index"
   get "/pets/new", to: "pets#new", as: 'new_pet'
-  get "/pets/card", to: "pets#card", as: 'card'
+  get "/pets/card", to: "pets#card", as: 'card' 
   get "/pets/:id", to: "pets#show", as: 'page'
-  delete "/pets/:id", to: "pets#destroy"
+  # delete "/pets/:id", to: "pets#destroy", as: 'delete_pet'
+  # post "/pets/:id", to: "pets#edit"
   resources :pets
   # Defines the root path route ("/")
   # root "articles#index"
