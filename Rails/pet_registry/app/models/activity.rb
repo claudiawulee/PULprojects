5e075070e10b239d005446bd8844bd7ef9ee3fb7
claudiawulee <cl7359@princeton.edu>
2023-08-29 21:30:24 +0200

    updating checkboxes

--- Rails/pet_registry/app/models/activity.rb
+++ Rails/pet_registry/app/models/activity.rb
@@ -1,6 +1,6 @@
 class Activity < ApplicationRecord
     validates :action, presence: true
-    validates :high_energy, presence: true
+    # validates :high_energy, presence: true
 
     has_and_belongs_to_many :pets
 
